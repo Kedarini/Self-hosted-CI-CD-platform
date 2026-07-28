5a2a422a69019fa88855307f28de41be43ca6f46
Kedarini <kedardamps@gmail.com>
2026-07-28 11:33:55 +0200

    Add additional output that print out SSH command to copy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,11 @@ output "ec2_public_ip" {
 }
 
 output "rds_endpoint" {
-    description = "RDS connection endpoint"
-    value = aws_db_instance.main.endpoint
+  description = "RDS connection address"
+  value       = aws_db_instance.main.address
+}
+
+output "ec2_ssh_command" {
+  description = "SSH connection command"
+  value       = "ssh -i ~/.ssh/id_ed25519 ubuntu@${aws_eip.app.public_ip}"
 }
