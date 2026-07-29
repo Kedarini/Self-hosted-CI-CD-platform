ff989b0d11c2099074634d97039ef3f6573bdb01
Kedarini <kedardamps@gmail.com>
2026-07-29 11:45:03 +0200

    separate password for grafana

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,6 +4,12 @@ variable "db_password" {
   sensitive   = true
 }
 
+variable "grafana_password" {
+  description = "Password for the Grafana dashboard"
+  type        = string
+  sensitive   = true
+}
+
 variable "allowed_ssh_cidr" {
   description = "CIDR block allowed to SSH into the app instance"
   type        = string
