3589438f5c2ca9fc7c2f04a5fce2be6bbd518442
Kedarini <kedardamps@gmail.com>
2026-07-29 11:38:06 +0200

    add SSH allowed ip's variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,8 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
+
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed to SSH into the app instance"
+  type        = string
+}
